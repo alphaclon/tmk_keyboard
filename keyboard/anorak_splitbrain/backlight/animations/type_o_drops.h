bc66d950bea561a2394af41406f4ae271b33e036
wenkm <MoritzWenk@web.de>
2017-04-12 17:14:55 +0200

    Fixed splitbrain reconnect after disconnect. Added another animation.

---
+++ keyboard/anorak_splitbrain/backlight/animations/type_o_drops.h
@@ -0,0 +1,12 @@
+
+#ifndef KEYBOARD_ANORAK_SPLITBRAIN_BACKLIGHT_ANIMATIONS_TYPE_O_DROPS_H
+#define KEYBOARD_ANORAK_SPLITBRAIN_BACKLIGHT_ANIMATIONS_TYPE_O_DROPS_H
+
+#include "matrix.h"
+
+void type_o_drops_animation_start(void);
+void type_o_drops_animation_loop(void);
+void type_o_drops_animation_stop(void);
+void type_o_drops_typematrix_row(uint8_t row_number, matrix_row_t row);
+
+#endif /* KEYBOARD_ANORAK_SPLITBRAIN_BACKLIGHT_ANIMATIONS_TYPE_O_DROPS_H */
